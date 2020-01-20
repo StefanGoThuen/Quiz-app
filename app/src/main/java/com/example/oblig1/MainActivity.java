4320cc55272212915b793d5a9457b6e3e3c9ff6a
h571725 <stefthue58@gmail.com>
2020-01-20 10:34:19 +0100

    addet knapper

--- app/src/main/java/com/example/oblig1/MainActivity.java
+++ app/src/main/java/com/example/oblig1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.oblig1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onclickData(View view) {
+    }
+
+    public void onclickQuiz(View view) {
+    }
+
+    public void onclickAdd(View view) {
+    }
+
 }
