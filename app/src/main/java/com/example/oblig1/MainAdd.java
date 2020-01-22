b7a8190d0a6694855bbfa91b6b1c1d33e6b5f5c4
Ole Rambech <olerambech@hotmail.com>
2020-01-22 10:57:47 +0100

    Legge til nye navn. Nye navn forsvinner når man går tilbake

--- app/src/main/java/com/example/oblig1/MainAdd.java
+++ app/src/main/java/com/example/oblig1/MainAdd.java
@@ -1,6 +1,8 @@
 package com.example.oblig1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,24 @@ import java.util.ArrayList;
 
 public class MainAdd extends AppCompatActivity {
     private ArrayList<String> navn;
-
+    private TextView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         navn = getIntent().getStringArrayListExtra("navn");
+        view = findViewById(R.id.textView);
+        for(String s : navn){
+            append(s);
+
+        }
+    }
+    public void addNew(View v){
+        EditText txt = findViewById(R.id.editText);
+        append(txt.getText().toString());
+        txt.setText("");
+    }
+    private void append(String s){
+        view.append(s + "\n");
     }
 }
