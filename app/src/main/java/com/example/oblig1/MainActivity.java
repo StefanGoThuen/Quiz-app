33c18cab8232557e66dbfdd4ee84d9cf4a716b29
Ole Rambech <olerambech@hotmail.com>
2020-01-22 11:57:06 +0100

    nye navn er nå persistent med Sharedpereferences. bilder mangler fortsatt

--- app/src/main/java/com/example/oblig1/MainActivity.java
+++ app/src/main/java/com/example/oblig1/MainActivity.java
@@ -3,18 +3,21 @@ package com.example.oblig1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> navn;
+    private SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(navn == null){
+        pref = getSharedPreferences("names", MODE_PRIVATE);
+        int size = pref.getAll().size();
+        if(size == 0){
             addNames();
         }
     }
@@ -31,14 +34,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void onclickAdd(View view) {
         Intent intent = new Intent(this, MainAdd.class);
-        intent.putExtra("navn", navn);
         startActivity(intent);
     }
     public void addNames(){
+        SharedPreferences.Editor editor = pref.edit();
         String[] names = getResources().getStringArray(R.array.navn);
-        navn = new ArrayList<String>(names.length);
-        for(int i = 0; i < names.length; i++){
-            navn.add(names[i]);
+
+        for(String s : names){
+            editor.putString(s, s);
         }
+        editor.commit();
     }
 }
--- app/src/main/java/com/example/oblig1/MainAdd.java
+++ app/src/main/java/com/example/oblig1/MainAdd.java
@@ -1,5 +1,6 @@
 package com.example.oblig1;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -8,29 +9,37 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class MainAdd extends AppCompatActivity {
-    private ArrayList<String> navn;
     private TextView view;
+    SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        navn = getIntent().getStringArrayListExtra("navn");
         view = findViewById(R.id.textView);
-        for(String s : navn){
-            append(s);
+        pref = getSharedPreferences("names", MODE_PRIVATE);
+        showNames(view);
 
-        }
     }
     public void addNew(View v){
         EditText txt = findViewById(R.id.editText);
         String nametoAdd = txt.getText().toString();
         append(nametoAdd);
-        navn.add(nametoAdd);
         txt.setText("");
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(nametoAdd, nametoAdd);
+        editor.commit();
     }
     private void append(String s){
         view.append(s + "\n");
+    }
+    private void showNames(View v){
+        Map names = pref.getAll();
+        Object[] s = names.values().toArray();
+        for(Object o : s){
+            view.append(o.toString() + "\n");
+        }
     }
 }
