82d7ea7aad8e2ed5a5ac549313946c4e479a6658
Ole Rambech <olerambech@hotmail.com>
2020-01-22 13:29:26 +0100

    Kamera implementert. Thumbnail blir returnert i activity men ikke lagret

--- app/src/main/java/com/example/oblig1/MainAdd.java
+++ app/src/main/java/com/example/oblig1/MainAdd.java
@@ -1,9 +1,13 @@
 package com.example.oblig1;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,8 @@ import java.util.Map;
 
 public class MainAdd extends AppCompatActivity {
     private TextView view;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView;
     SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,7 @@ public class MainAdd extends AppCompatActivity {
         setContentView(R.layout.activity_add);
         view = findViewById(R.id.textView);
         pref = getSharedPreferences("names", MODE_PRIVATE);
+        imageView = findViewById(R.id.imageView);
         showNames(view);
 
     }
@@ -40,6 +47,21 @@ public class MainAdd extends AppCompatActivity {
         Object[] s = names.values().toArray();
         for(Object o : s){
             view.append(o.toString() + "\n");
+        }
+    }
+    public void startPicture(View v){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            imageView.setImageBitmap(imageBitmap);
         }
     }
 }
